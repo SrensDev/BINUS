cc8bbfa8c56aaa5252db042ba8ef9af4e109149f
Srens <arybudianto0836@gmail.com>
2025-09-03 15:13:15 +0700

    Tugas sesi 3

--- README.md
+++
@@ -1,2 +0,0 @@
-# LOREM IPSUM
-lorem ipsum
---
+++ Sesi 3 - Git Started/Introduction.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, my name is Ary Budianto with Student ID: 2902616715\nMy favorite programming language is Python, C++ and JavaScript.");
+
+    return 0;
+}
